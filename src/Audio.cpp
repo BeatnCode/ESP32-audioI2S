18d089de08794dfede4fe7858e65beafb8968d35
BeatnCode <BeatnCode@icloud.com>
2026-02-03 22:09:06 +0100

    turn lowshelf into highpass

--- src/Audio.cpp
+++ src/Audio.cpp
@@ -2378,7 +2378,7 @@ void Audio::playChunk() {
             IIR_filterChain1(*sample);
             IIR_filterChain2(*sample);
         }
-        
+
         //---------- Stereo -> Mono  ---------------------------------------
         if (m_f_forceMono && m_channels == 2) {
             s2 = *sample;
@@ -5286,27 +5286,40 @@ void Audio::IIR_calculateCoefficients(int8_t G0, int8_t G1, int8_t G2) { // Infi
     }
     float K, norm, Q, Fc, V;
 
-    // LOWSHELF
-    Fc = (float)FcLS / (float)getSampleRate(); // Cutoff frequency
+    // LOWSHELF -> HIGHPASS
+    Q = 1.0 + (1.0 * G0) / 10;      // Quality factor
+    if (Q < 0.1) Q = 0.1;           // limit Q to minimum 0.1 to avoid extreme filter settings
+    Fc = (float)FcHP / (float)getSampleRate(); // Cutoff frequency
     K = tanf((float)PI * Fc);
-    V = powf(10, fabs(G0) / 20.0);
 
-    if(G0 >= 0) { // boost
-        norm = 1 / (1 + sqrtf(2) * K + K * K);
-        m_filter[LOWSHELF].a0 = (1 + sqrtf(2 * V) * K + V * K * K) * norm;
-        m_filter[LOWSHELF].a1 = 2 * (V * K * K - 1) * norm;
-        m_filter[LOWSHELF].a2 = (1 - sqrtf(2 * V) * K + V * K * K) * norm;
-        m_filter[LOWSHELF].b1 = 2 * (K * K - 1) * norm;
-        m_filter[LOWSHELF].b2 = (1 - sqrtf(2) * K + K * K) * norm;
-    }
-    else { // cut
-        norm = 1 / (1 + sqrtf(2 * V) * K + V * K * K);
-        m_filter[LOWSHELF].a0 = (1 + sqrtf(2) * K + K * K) * norm;
-        m_filter[LOWSHELF].a1 = 2 * (K * K - 1) * norm;
-        m_filter[LOWSHELF].a2 = (1 - sqrtf(2) * K + K * K) * norm;
-        m_filter[LOWSHELF].b1 = 2 * (V * K * K - 1) * norm;
-        m_filter[LOWSHELF].b2 = (1 - sqrtf(2 * V) * K + V * K * K) * norm;
-    }
+    norm = 1 / (1 + K / Q + K * K);
+    m_filter[LOWSHELF].a0 = 1 * norm;
+    m_filter[LOWSHELF].a1 = -2 * 1 * norm;
+    m_filter[LOWSHELF].a2 = 1 * norm;
+    m_filter[LOWSHELF].b1 = 2 * (K * K - 1) * norm;
+    m_filter[LOWSHELF].b2 = (1 - K / Q + K * K) * norm;
+
+    // // LOWSHELF
+    // Fc = (float)FcLS / (float)getSampleRate(); // Cutoff frequency
+    // K = tanf((float)PI * Fc);
+    // V = powf(10, fabs(G0) / 20.0);
+
+    // if(G0 >= 0) { // boost
+    //     norm = 1 / (1 + sqrtf(2) * K + K * K);
+    //     m_filter[LOWSHELF].a0 = (1 + sqrtf(2 * V) * K + V * K * K) * norm;
+    //     m_filter[LOWSHELF].a1 = 2 * (V * K * K - 1) * norm;
+    //     m_filter[LOWSHELF].a2 = (1 - sqrtf(2 * V) * K + V * K * K) * norm;
+    //     m_filter[LOWSHELF].b1 = 2 * (K * K - 1) * norm;
+    //     m_filter[LOWSHELF].b2 = (1 - sqrtf(2) * K + K * K) * norm;
+    // }
+    // else { // cut
+    //     norm = 1 / (1 + sqrtf(2 * V) * K + V * K * K);
+    //     m_filter[LOWSHELF].a0 = (1 + sqrtf(2) * K + K * K) * norm;
+    //     m_filter[LOWSHELF].a1 = 2 * (K * K - 1) * norm;
+    //     m_filter[LOWSHELF].a2 = (1 - sqrtf(2) * K + K * K) * norm;
+    //     m_filter[LOWSHELF].b1 = 2 * (V * K * K - 1) * norm;
+    //     m_filter[LOWSHELF].b2 = (1 - sqrtf(2 * V) * K + V * K * K) * norm;
+    // }
 
     // PEAK EQ
     Fc = (float)FcPKEQ / (float)getSampleRate(); // Cutoff frequency
